f9267dee208fbfde9406824ce560173bd4fe478b
Cameron Graham <cameron.g882@gmail.com>
2020-08-17 00:17:04 -0400

    fixed server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -25,5 +25,5 @@ mongoose.connect("mongodb://camgram:hello9@ds213209.mlab.com:13209/heroku_42jhcs
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log("App running: ==> 🌎 Visit http://localhost:3000/ 🌎 <== ");
 });
